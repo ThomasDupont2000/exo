7f7d435f9d551933fbad5693aa56d8a7d665d4c6
ITGA\tco <thomas.chategnier@itga.fr>
2025-09-18 08:49:58 +0200

    Added asyncPipe to component3

--- src/app/features/exo2/components/component3/component3.ts
+++ src/app/features/exo2/components/component3/component3.ts
@@ -1,20 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { LetterIndexStoreService } from '../../letter-index-store.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-component3',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './component3.html',
   styleUrl: './component3.scss'
 })
 export class Component3 {
   private readonly letterIndexStoreService = inject(LetterIndexStoreService);
   
-  receivedLetter = '';
-
-  constructor () {
-    this.letterIndexStoreService.getLetterSubjectObs().subscribe((letter: string) => {
-      this.receivedLetter = letter;
-    });
-  }
+  readonly receivedLetter$ = this.letterIndexStoreService.getLetterSubjectObs();
 }
